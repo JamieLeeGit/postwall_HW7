95ed798641dbc47f7ffeec0845979b2e8579b3f4
jamie <b95170080@gmail.com>
2022-06-07 04:01:07 +0800

    add postman

--- routes/upload.js
+++ routes/upload.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const handleErrorAsync = require("../service/handleErrorAsync");
 const handleSuccess = require("../service/handleSuccess");
 const httpStatus = require("../service/httpStatus");
+const { isAuth } = require('../middleware/auth');
 const uploadImage = require('../service/uploadImage');
 const saveImage = require('../service/saveImage');
 
 router
   .post(
     '/', 
+    isAuth,
     uploadImage, 
     handleErrorAsync(async (req, res, next)=> {
-      console.log('handleErrorAsync');
       const data =  await saveImage(req, res, next);
       handleSuccess(res, httpStatus.OK, data);
   }));
